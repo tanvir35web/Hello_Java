84fb5b72f62a5f3a043591aab24f8347666558d9
tanvir35web <islammdtanvirul@gmail.com>
2024-11-01 14:32:50 +0600

    added merged sorted arrays problem using array and list both in java

--- src/MergeSortedArrays.java
+++ src/MergeSortedArrays.java
@@ -1,42 +1,43 @@
 public class MergeSortedArrays {
-    public static void main(String[] args) {
-        int[] arr1 = {1, 3, 5, 7};
-        int[] arr2 = {2, 4, 6, 8, 9};
+    public static int[] mergeSortedArrays(int[] array1, int[] array2) {
+        int lengthOfArray1 = array1.length;
+        int lengthOfArray2 = array2.length;
+        int[] mergedArray = new int[lengthOfArray1 + lengthOfArray2];
 
-        int[] mergedArray = mergeSortedArrays(arr1, arr2);
+        int i = 0, j = 0, k = 0;
+
+        // Traverse both arrays and insert the smaller element into the merged array
+        while (i < lengthOfArray1 && j < lengthOfArray2) {
+            if (array1[i] < array2[j]) {
+                mergedArray[k++] = array1[i++];
+            } else {
+                mergedArray[k++] = array2[j++];
+            }
+        }
+
+        // Copy remaining elements of arr1, if any
+        while (i < lengthOfArray1) {
+            mergedArray[k++] = array1[i++];
+        }
+
+        // Copy remaining elements of arr2, if any
+        while (j < lengthOfArray2) {
+            mergedArray[k++] = array2[j++];
+        }
+
+        return mergedArray;
+    };
+    public static void main(String[] args) {
+        int[] array1 = {7, 9, 10, 13, 19, 25};
+        int[] array2 = {1, 3, 5, 8, 11, 15, 16, 17, 20, 50 };
+
+        int[] mergedArray = mergeSortedArrays(array1, array2);
 
         System.out.print("Merged array: ");
         for (int num : mergedArray) {
             System.out.print(num + " ");
         }
-    }
 
-    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
-        int n1 = arr1.length;
-        int n2 = arr2.length;
-        int[] merged = new int[n1 + n2];
-
-        int i = 0, j = 0, k = 0;
-
-        // Traverse both arrays and insert the smaller element into the merged array
-        while (i < n1 && j < n2) {
-            if (arr1[i] < arr2[j]) {
-                merged[k++] = arr1[i++];
-            } else {
-                merged[k++] = arr2[j++];
-            }
-        }
-
-        // Copy remaining elements of arr1, if any
-        while (i < n1) {
-            merged[k++] = arr1[i++];
-        }
-
-        // Copy remaining elements of arr2, if any
-        while (j < n2) {
-            merged[k++] = arr2[j++];
-        }
-
-        return merged;
     }
 }
+
