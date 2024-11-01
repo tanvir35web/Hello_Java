4cadc492d26ce919eba2484de2b5f97292af2e8f
tanvir35web <islammdtanvirul@gmail.com>
2024-11-01 12:23:58 +0600

    find max from array problem added

---
+++ src/findMax.java
@@ -0,0 +1,15 @@
+public class findMax {
+    public static void main(String[] args) {
+        int[] numbers = {2,9,0,8,99,-8,1};
+
+        int max = numbers[0];
+
+        for(int i = 1; i <numbers.length; i++) {
+            if (max < numbers[i]){
+                max = numbers[i];
+            }
+        }
+        System.out.println("The max value is " + max);
+
+    }
+}
