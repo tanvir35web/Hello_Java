d8ac37f0edf31e0e90cade19e208676b6853d577
tanvir35web <islammdtanvirul@gmail.com>
2024-11-02 18:43:27 +0600

    factiorial problem solved using recursion

---
+++ src/Factorial.java
@@ -0,0 +1,14 @@
+class Factorial {
+    public static void main(String[] args){
+        Factorial recursion = new Factorial();
+        var result = recursion.factorial(5);
+        System.out.println(result);
+    }
+
+    public int factorial(int n){
+        if (n==0 || n==1){
+            return 1;
+        }
+    return n * factorial(n-1);
+    };
+}
