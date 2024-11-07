b7a7462127453c2362d7d394320f86878d22df17
tanvir35web <islammdtanvirul@gmail.com>
2024-11-07 09:25:54 +0600

    fibonacci solved using dynamic programmin with O(n) complexity

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,15 +1,20 @@
-class Fibonacci {
-    public static void main(String[] args) {
-        Fibonacci fibonacciCalculator = new Fibonacci();
-        var result = fibonacciCalculator.calculateFibonacci(4);
-        System.out.println(result);
-    }
-
-    public int calculateFibonacci(int n) {
+public class Fibonacci {
+    public static int calculateFibonacci(int n) {
         if (n < 0) return -1;
-        if (n == 0 || n == 1) {
-            return n;
+        if (n==1 || n==2) return 1;
+
+        int[] fib = new int[n+1];
+        fib[1] = 1;
+        fib[2] = 1;
+        for (int i=3; i <= n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
         }
-        return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+        return fib[n];
+
+    }
+    public static void main(String[] args) {
+        int n = 6;
+        int result = calculateFibonacci(n);
+        System.out.println("Fibonacci of " + n + " is " + result);
     }
 }
